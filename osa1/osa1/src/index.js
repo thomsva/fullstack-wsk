e4ec22976517f33b16935c4d3ad8ead2c3458c6d
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2018-01-29 20:01:44 +0200

    1.2 valmis

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -30,20 +30,26 @@ const Osa = (props) => {
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osa1 = {
+    nimi: 'Reactin perusteet',
+    tehtavia: 10
+  }
+  const osa2 = {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7
+  }
+  const osa3 = {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14
+  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
       <div>
-        <Sisalto osa={osa1} tehtavia={tehtavia1} />
-        <Sisalto osa={osa2} tehtavia={tehtavia2} />
-        <Sisalto osa={osa3} tehtavia={tehtavia3} />
+        <Sisalto osa={osa1.nimi} tehtavia={osa1.tehtavia} />
+        <Sisalto osa={osa2.nimi} tehtavia={osa2.tehtavia} />
+        <Sisalto osa={osa3.nimi} tehtavia={osa3.tehtavia} />
         
       </div>
     </div>
