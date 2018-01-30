428f0175214892e34fbaf1cb0fbf40519494a689
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2018-01-30 13:47:43 +0200

    1.12 aloitettu

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -5,87 +5,29 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      hyva: 0,
-      neutraali: 0,
-      huono: 0,
-      total: 0,
+      selected: 0
     }
   }
-
-  klikButton = (name, value) => {
-    if (name==='hyva') {
-      return () => {
-        this.setState({
-          hyva: value,
-          total: this.state.total + 1,
-        })
-      }
-    }
-    if (name==='neutraali') {
-      return () => {
-        this.setState({
-          neutraali: value,
-          total: this.state.total + 1,
-        })
-      }
-    }
-    if (name==='huono') {
-      return () => {
-        this.setState({
-          huono: value,
-          total: this.state.total + 1,
-        })
-      }
-    }
-  }
-
 
   render() {
     return (
       <div>
-        <div>
-          <h1>anna palautetta</h1>
-          <Button text="hyvä" klik={this.klikButton('hyva',this.state.hyva+1)} />
-          <Button text="neutraali" klik={this.klikButton('neutraali',this.state.neutraali+1)} />
-          <Button text="huono" klik={this.klikButton('huono',this.state.huono+1)} />
-          <Statistics stats={this.state} />
-        </div>
-
+        {this.props.anecdotes[this.state.selected]}
       </div>
     )
   }
 }
 
-
-const Button = ({text, klik}) => <button onClick={klik}>{text}</button>
-
-const Statistic = ({ name, value}) => <tr><td>{name}</td><td>{value}</td></tr>
-
-const Statistics = ({stats}) => {
-  if (stats.hyva === 0 && stats.huono === 0 && stats.neutraali === 0) {
-    return (
-      <div>
-       <p>Ei vielä annettuja palautteita!</p>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <h1>tilastot</h1>
-      <table>
-        <tbody>
-          <Statistic name="hyvä" value={stats.hyva} />
-          <Statistic name="neutraali" value={stats.neutraali} />
-          <Statistic name="huono" value={stats.huono} />
-          <Statistic name="keskiarvo" value={(stats.hyva-stats.huono)/stats.total} />
-          <Statistic name="positiivisia" value={100*stats.hyva/stats.total +' %'} /> 
-        </tbody>
-      </table>
-    </div>
-  )
-}
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <App />,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
