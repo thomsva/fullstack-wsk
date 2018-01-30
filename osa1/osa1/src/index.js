3df7f29b2f9a5ab787acccc0eb1174798c85cf88
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2018-01-30 13:28:16 +0200

    1.10 valmis

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -9,46 +9,45 @@ class App extends React.Component {
       neutraali: 0,
       huono: 0,
       total: 0,
-      average: 0,
-      positive: 0
     }
   }
 
-  klikHyva = () => {
-    this.setState({
-      hyva: this.state.hyva + 1,
-      total: this.state.total +1,
-      positive: (this.state.hyva+1)/(this.state.total+1),
-      average: (this.state.huono*-1 + this.state.hyva+1)/(this.state.total+1)
-    })
+  klikButton = (name, value) => {
+    if (name==='hyva') {
+      return () => {
+        this.setState({
+          hyva: value,
+          total: this.state.total + 1,
+        })
+      }
+    }
+    if (name==='neutraali') {
+      return () => {
+        this.setState({
+          neutraali: value,
+          total: this.state.total + 1,
+        })
+      }
+    }
+    if (name==='huono') {
+      return () => {
+        this.setState({
+          huono: value,
+          total: this.state.total + 1,
+        })
+      }
+    }
   }
 
-  klikNeutraali = () => {
-    this.setState({
-      neutraali: this.state.neutraali + 1,
-      total: this.state.total +1,
-      positive: (this.state.hyva)/(this.state.total+1),
-      average: (this.state.huono*-1 + this.state.hyva)/(this.state.total+1)
-    })
-  }
-
-  klikHuono = () => {
-    this.setState({
-      huono: this.state.huono + 1,
-      total: this.state.total +1,
-      positive: (this.state.hyva)/(this.state.total+1),
-      average: ((this.state.huono+1)*-1 + this.state.hyva)/(this.state.total+1)
-    })
-  }
 
   render() {
     return (
       <div>
         <div>
           <h1>anna palautetta</h1>
-          <Button text="hyvä" klik={this.klikHyva} />
-          <Button text="neutraali" klik={this.klikNeutraali} />
-          <Button text="huono" klik={this.klikHuono} />
+          <Button text="hyvä" klik={this.klikButton('hyva',this.state.hyva+1)} />
+          <Button text="neutraali" klik={this.klikButton('neutraali',this.state.neutraali+1)} />
+          <Button text="huono" klik={this.klikButton('huono',this.state.huono+1)} />
           <Statistics stats={this.state} />
         </div>
 
@@ -56,6 +55,7 @@ class App extends React.Component {
     )
   }
 }
+
 
 const Button = ({text, klik}) => <button onClick={klik}>{text}</button>
 
@@ -75,8 +75,8 @@ const Statistics = ({stats}) => {
       <Statistic name="hyvä" value={stats.hyva} />
       <Statistic name="neutraali" value={stats.neutraali} />
       <Statistic name="huono" value={stats.huono} />
-      <Statistic name="keskiarvo" value={stats.average} />
-      <Statistic name="positiivisia" value={100*stats.positive+' %'} /> 
+      <Statistic name="keskiarvo" value={(stats.hyva-stats.huono)/stats.total} />
+      <Statistic name="positiivisia" value={100*stats.hyva/stats.total +' %'} /> 
     </div>
   )
 }
