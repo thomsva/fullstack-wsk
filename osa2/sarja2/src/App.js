d50887f8b21c1d2920f87fad51381fba6cda9b46
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2018-02-05 19:17:45 +0200

    2.9 valmis

--- osa2/sarja2/src/App.js
+++ osa2/sarja2/src/App.js
@@ -5,11 +5,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas', number: '123456'},
-        { name: 'Timo Testaaja', number: '987654'}
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
       ],
       newName: '',
-      newNumber: ''
+      newNumber: '',
+      filter: ''
     }
   }
 
@@ -37,19 +40,24 @@ class App extends React.Component {
   }
 
   handleNameChange = (event) => {
-    console.log(event.target.value)
     this.setState({ newName: event.target.value })
   }
 
   handleNumberChange = (event) => {
-    console.log(event.target.value)
     this.setState({ newNumber: event.target.value })
+  }
+
+  handleFilterChange = (event) => {
+    const filter =event.target.value
+    this.setState({ filter: filter })
   }
 
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
+        
+
         <form onSubmit={this.addPerson}>
           <div>
             nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
@@ -61,14 +69,18 @@ class App extends React.Component {
             <button type="submit">lisää</button>
           </div>
         </form>
-        <h2>Numerot</h2>
-        <table>
-        
-          {this.state.persons.map(person => <tr key={person.name}><td> {person.name}</td><td> {person.number} </td></tr>)}
-        </table>
+        <h2>Kaikki Numerot</h2>
         <div>
-          debug: {this.state.newName}
+           rajaa tuloksia: <input value={this.state.filter} onChange={this.handleFilterChange} />
         </div>
+        <table>
+          <tbody>
+            {this.state.persons.filter(person => person.name.includes(this.state.filter)).map(person => <tr key={person.name}><td> {person.name}</td><td> {person.number} </td></tr>)}
+          </tbody>
+        </table>
+
+
+
       </div>
     )
   }
